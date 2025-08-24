f4be7eb5a1958b693383f32ca01045e056172046
JunhaoW3 <wupillow@gmail.com>
2025-08-24 17:39:00 +0800

    Added echo feature for bot

--- src/main/java/Alice.java
+++ src/main/java/Alice.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Alice {
     public static String botName = "Alice";
 
@@ -21,12 +23,26 @@ public class Alice {
     }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         horizontalLine();
         System.out.println("Hello! I'm " + getBotName());
         System.out.println("What can I do for you?");
         horizontalLine();
 
+        //Using lower case so that it accepts different iterations of "bye"
+        String text = scanner.nextLine();
+        String lowerCase = text.toLowerCase();
+
+        while (!lowerCase.equals("bye")) {
+            horizontalLine();
+            System.out.println(text);
+            horizontalLine();
+            text = scanner.nextLine();
+            lowerCase = text.toLowerCase();
+        }
+
+        horizontalLine();
         exit();
     }
 }
